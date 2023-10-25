a3650e1a6d6b8e67ff7ec2da03b852855d4809e6
LaisLatifi <mudaser1992@gmail.com>
2023-10-24 17:29:40 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("i am batch 16");
+        System.out.println(" we have sucesfuly finished all SDET");
+
+    }
+}
